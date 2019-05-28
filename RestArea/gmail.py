f01f25315f811e9ade89ab90c1b955a588fd9a21
minjoo <minf0723@naver.com>
2019-05-28 23:33:11 +0900

    send nontext mail
    send nontext mail

--- RestArea/gmail.py
+++ RestArea/gmail.py
@@ -7,50 +7,6 @@ from http.client import HTTPSConnection
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
-def sendGmail():
-    global host, port
-    html = ""
-    title = str(input ('Title :'))
-    senderAddr = str(input ('sender email address :'))
-    recipientAddr = str(input ('recipient email address :'))
-    msgtext = str(input ('write message :'))
-    passwd = str(input (' input your password of gmail account :'))
-    msgtext = str(input ('Do you want to include book data (y/n):'))
-    if msgtext == 'y' :
-        keyword = str(input ('input keyword to search:'))
-        html = MakeHtmlDoc(SearchBookTitle(keyword))
-
-    import mysmtplib
-    # MIMEMultipart의 MIME을 생성합니다.
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
-
-    #Message container를 생성합니다.
-    msg = MIMEMultipart('alternative')
-
-    #set message
-    msg['Subject'] = title
-    msg['From'] = senderAddr
-    msg['To'] = recipientAddr
-
-    msgPart = MIMEText(msgtext, 'plain')
-    bookPart = MIMEText(html, 'html', _charset = 'UTF-8')
-
-    # 메세지에 생성한 MIME 문서를 첨부합니다.
-    msg.attach(msgPart)
-    msg.attach(bookPart)
-
-    print ("connect smtp server ... ")
-    s = mysmtplib.MySMTP(host,port)
-    #s.set_debuglevel(1)
-    s.ehlo()
-    s.starttls()
-    s.ehlo()
-    s.login(senderAddr, passwd)    # 로긴을 합니다.
-    s.sendmail(senderAddr , [recipientAddr], msg.as_string())
-    s.close()
-
-    print ("Mail sending complete!!!")
 
 #global value
 host = "smtp.gmail.com" # Gmail STMP 서버 주소.
@@ -66,19 +22,53 @@ msg['From'] = senderAddr
 msg['To'] = recipientAddr
 
 # MIME 문서를 생성합니다.
-htmlFD = open(htmlFileName, 'rb')
-HtmlPart = MIMEText(htmlFD.read(),'html', _charset = 'UTF-8' )
-htmlFD.close()
+#htmlFD = open(htmlFileName, 'rb')
+#HtmlPart = MIMEText(htmlFD.read(),'html', _charset = 'UTF-8' )
+#htmlFD.close()
 
-# 만들었던 mime을 MIMEBase에 첨부 시킨다.
-msg.attach(HtmlPart)
+def sendGmail():
+    global host, port
+    html = ""
+    title = 'FUCK'
+    senderAddr = 'min164337@gmail.com'
+    recipientAddr = 'minf0723@naver.com'
+    msgtext = 'fuck you'
+    passwd = '88088808!a'
+    #msgtext = str(input ('Do you want to include book data (y/n):'))
+    msgtext = 'n'
+    if msgtext == 'y' :
+        keyword = str(input ('input keyword to search:'))
+        #html = MakeHtmlDoc(SearchBookTitle(keyword))
+        html = 'hello'
 
-# 메일을 발송한다.
-s = mysmtplib.MySMTP(host,port)
-#s.set_debuglevel(1)        # 디버깅이 필요할 경우 주석을 푼다.
-s.ehlo()
-s.starttls()
-s.ehlo()
-s.login("min164337@gmail.com","88088808!a")
-s.sendmail(senderAddr , [recipientAddr], msg.as_string())
-s.close()
+    import mysmtplib
+    # MIMEMultipart의 MIME을 생성합니다.
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
+    #Message container를 생성합니다.
+    msg = MIMEMultipart('alternative')
+
+    #set message
+    msg['Subject'] = title
+    msg['From'] = senderAddr
+    msg['To'] = recipientAddr
+
+    msgPart = MIMEText(msgtext, 'plain')
+    #bookPart = MIMEText(html, 'html', _charset = 'UTF-8')
+
+    # 메세지에 생성한 MIME 문서를 첨부합니다.
+    msg.attach(msgPart)
+    #msg.attach(bookPart)
+
+    print ("connect smtp server ... ")
+    s = mysmtplib.MySMTP(host,port)
+    #s.set_debuglevel(1)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login(senderAddr, passwd)    # 로긴을 합니다.
+    s.sendmail(senderAddr , [recipientAddr], msg.as_string())
+    s.close()
+
+    print ("Mail sending complete!!!")
